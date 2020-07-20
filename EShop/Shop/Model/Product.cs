dc224da2d27a51149db45002b85ace746304c770
AntonNikolaev <34309871+AntonNikolaevKazan@users.noreply.github.com>
2020-07-20 18:58:18 +0300

    Add MVC architecture

---
+++ EShop/Shop/Model/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Model
+{
+    // Базовый класс для всех продуктов в магазине.
+    public abstract class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public int Price { get; set; }
+    }
+}
