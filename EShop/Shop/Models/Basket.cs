22230f45c6261ff21098c4b5198eec11f351c743
AntonNikolaev <34309871+AntonNikolaevKazan@users.noreply.github.com>
2020-07-21 02:39:59 +0300

    Create basket model

--- EShop/Shop/Models/Basket.cs
+++ EShop/Shop/Models/Basket.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Shop.Model;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +10,26 @@ namespace Shop.Models
     // Модель корзины с товарами покупателя.
     public class Basket
     {
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int BasketId { get; set; }
 
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int ProductId { get; set; } // Ссылка на связанную модель Product.
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int ProductCountInBasket { get; set; } // Количество в корзине товара (штук).
+
+        [Required]
+        public Product Product { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int CustomerId { get; set; } // Ссылка на связанную модель Customer.
+
+        [Required]
+        public Customer Customer { get; set; }
     }
 }
