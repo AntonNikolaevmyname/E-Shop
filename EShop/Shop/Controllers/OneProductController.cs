e9165c803d97ffc169112df9a9935fab280e4d05
AntonNikolaev <34309871+AntonNikolaevKazan@users.noreply.github.com>
2020-07-28 02:27:38 +0300

    Add one product page

--- EShop/Shop/Controllers/OneProductController.cs
+++ EShop/Shop/Controllers/OneProductController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Shop.Model;
 
 namespace Shop.Controllers
 {
     public class OneProductController : Controller
     {
-        public IActionResult Index()
+        ShopContext db;
+        public OneProductController(ShopContext context)
         {
-            return View();
+            db = context;
+        }
+        public IActionResult OneProduct(int id)
+        {
+            return View(db.Products.Where(product => id == product.ProductId));
         }
     }
 }
