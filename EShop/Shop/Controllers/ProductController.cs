fbc1b7053ea78e56e22cc073ab454359a368d164
AntonNikolaev <34309871+AntonNikolaevKazan@users.noreply.github.com>
2020-07-28 02:14:20 +0300

    Create product page

--- EShop/Shop/Controllers/ProductController.cs
+++ EShop/Shop/Controllers/ProductController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Shop.Model;
 
 namespace Shop.Controllers
 {
     public class ProductController : Controller
     {
+        ShopContext db;
+        public ProductController(ShopContext context)
+        {
+            db = context;
+        }
+
         public IActionResult Product()
         {
-            return View();
+            return View(db.Products.ToList());
         }
     }
 }
